590657a5d9cd7360ccad0203d8f353874250ecab
JecaTatu <gas5@cin.ufpe.br>
2020-07-16 13:07:56 -0300

    connect to dialogFlow

--- apps/dialog_flow/views.py
+++ apps/dialog_flow/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.http import HttpResponse, JsonResponse
+
+import json
 
 # Create your views here.
+
+class webhookDialog(APIView):
+
+    def post(self, request, format=None):
+        # build a request object
+        req = json.loads(request.body)
+        # get action from json
+        action = req.get('queryResult').get('action')
+        # return a fulfillment message
+        fulfillmentText = {'fulfillmentText': 'This is Django test response from webhook.'}
+
+        return Response(fulfillmentText)
--- conf/djangotest/api_urls.py
+++ conf/djangotest/api_urls.py
@@ -1,1 +1,8 @@
-urlpatterns = []
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from apps.dialog_flow.views import webhookDialog
+
+urlpatterns = [
+    path('webhook/', webhookDialog.as_view(), name='webhook')
+]
